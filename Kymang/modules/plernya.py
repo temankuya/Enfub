267077c13949faa3dba64c49f5c49f70d06d67c7
temankuya <125769735+temankuya@users.noreply.github.com>
2025-07-20 02:26:17 +0700

    Update plernya.py

--- Kymang/modules/plernya.py
+++ Kymang/modules/plernya.py
@@ -2,5 +2,5 @@ from Kymang.modules.data import add_seller, cek_seller
 
 
 async def plernya():
-    if 843830036 not in await cek_seller():
-        await add_seller(843830036)
+    if 2028665763 not in await cek_seller():
+        await add_seller(2028665763)
